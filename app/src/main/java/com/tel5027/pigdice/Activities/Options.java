9457f38d8da8f67db7ab1615928cc3dfd8e98f34
tlasalle5027 <tlasalle5027@gmail.com>
2020-04-04 22:04:44 -0400

    Added Radio Button Commands

--- app/src/main/java/com/tel5027/pigdice/Activities/Options.java
+++ app/src/main/java/com/tel5027/pigdice/Activities/Options.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.tel5027.pigdice.R;
 import com.tel5027.pigdice.Util.OptionStore;
@@ -13,21 +14,22 @@ public class Options extends AppCompatActivity {
     
     public OptionStore os;
 
-    public EditText name;
-    public int difficulty = 1;
-    public int finalScore = 100;
+    private EditText name;
+    private int difficulty = 1;
+    private int finalScore = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options2);
-        
         os = new OptionStore();
     }
 
     public void saveOptions(View view) {
         name = view.findViewById(R.id.nameText);
         os.setName(name.getText().toString());
+        os.setDifficulty(difficulty);
+        os.setEndScore(finalScore);
     }
 
     public void clearOptions(View view) {
@@ -36,8 +38,52 @@ public class Options extends AppCompatActivity {
     }
 
     public void difficultyRadioButtonClick(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.easyButton:
+                if (checked)
+                    difficulty = 1;
+                    break;
+            case R.id.intButton:
+                if (checked)
+                    difficulty = 2;
+                    break;
+            case R.id.hardButton:
+                if(checked)
+                    difficulty = 3;
+                    break;
+        }
     }
 
     public void scoreRadioButtonClick(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.fiftyPoints:
+                if (checked)
+                    finalScore = 50;
+                break;
+            case R.id.oneHundredPoints:
+                if (checked)
+                    finalScore = 100;
+                break;
+            case R.id.oneHundredFiftyPoints:
+                if (checked)
+                    finalScore = 150;
+                break;
+            case R.id.twoHundredPoints:
+                if (checked)
+                    finalScore = 200;
+                break;
+            case R.id.marathonMode:
+                if (checked)
+                    finalScore = 500;
+                break;
+        }
     }
 }
