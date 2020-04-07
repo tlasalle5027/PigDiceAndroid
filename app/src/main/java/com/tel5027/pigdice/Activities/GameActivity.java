7bd4fc0f8e36c11ba5c108c3da4688dd8d9e744f
tlasalle5027 <tlasalle5027@gmail.com>
2020-04-07 13:50:45 -0400

    Started Writing GameLogic

--- app/src/main/java/com/tel5027/pigdice/Activities/GameActivity.java
+++ app/src/main/java/com/tel5027/pigdice/Activities/GameActivity.java
@@ -3,13 +3,16 @@ package com.tel5027.pigdice.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.tel5027.pigdice.Controller.GameLogic;
 import com.tel5027.pigdice.R;
 import com.tel5027.pigdice.Util.OptionStore;
 
 public class GameActivity extends AppCompatActivity {
 
     public OptionStore os;
+    private GameLogic engine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,15 @@ public class GameActivity extends AppCompatActivity {
         else{
             os = Options.os;
         }
+
+        engine = new GameLogic(os);
+    }
+
+    public void quitGame(View view) {
+        finish();
+    }
+
+    public void rollDice(View view) {
+        engine.playerTurn(view);
     }
 }
--- app/src/main/java/com/tel5027/pigdice/Controller/GameLogic.java
+++ app/src/main/java/com/tel5027/pigdice/Controller/GameLogic.java
@@ -1,8 +1,67 @@
 package com.tel5027.pigdice.Controller;
 
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.tel5027.pigdice.R;
+import com.tel5027.pigdice.Util.OptionStore;
+
+import java.util.Random;
+
 public class GameLogic {
 
-    public GameLogic(){
+    private Random pigroll;
+    private OptionStore os;
+    private int endScore;
+    private int difficulty;
+
+    public GameLogic(OptionStore o){
+        os = o;
+        pigroll = new Random();
+
+        difficulty = os.getDifficulty();
+        endScore = os.getEndScore();
+    }
+
+    public int turn(){
+        return pigroll.nextInt(6) + 1;
+    }
+
+    public boolean checkWinner(int pScore, int cScore){
+        return (pScore < endScore && cScore < endScore);
+    }
+
+    public int playerTurn(View v){
+        int roll = 0;
+        ImageView diceImage = (ImageView)v.findViewById(R.id.diceImage);
+
+        roll = turn();
+
+        switch(roll){
+            case 1:
+                diceImage.setImageResource(R.drawable.die_1);
+                roll = 0;
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.die_2);
+                break;
+            case 3:
+                diceImage.setImageResource(R.drawable.die_3);
+                break;
+            case 4:
+                diceImage.setImageResource(R.drawable.die_4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.die_5);
+                break;
+            case 6:
+                diceImage.setImageResource(R.drawable.die_6);
+                break;
+        }
+
+         return roll;
 
     }
+
 }
